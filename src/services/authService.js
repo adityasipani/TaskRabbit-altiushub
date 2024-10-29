1f5672c2904338d01b81f936ebb5344236a4d4a1
Aditya Sipani <asipani969@gmail.com>
2024-10-29 18:03:53 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const register=(data)=>axios.post('/api/register',data);
 export const login=(data)=>axios.post('/api/login',data);
-export const getToken=()=>localstorage.getItem('token');
-export const setToken=(token)=>localstorage.setItem('token',token);
-export const removeToken=()=>localstorage.removeItem('token');
+export const getToken=()=>localStorage.getItem('token');
+export const setToken=(token)=>localStorage.setItem('token',token);
+export const removeToken=()=>localStorage.removeItem('token');
